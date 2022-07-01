66fe06392efed93c81bef7e603a679efd92f0594
DeCaelo <ludovic.cleuet@gmail.com>
2022-07-01 16:02:11 +0200

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+enum RSEnum {
+    Foo(i32),
+    Bar(String),
+    Baz(Vec<String>),
+    Foo2(Option<i32>),
+}
+
 fn main() {
-    let a = vec![];
-    let mut b = a;
+    // let a = vec![];
+    // let mut b = a;
 
-    b.push(1);
+    // b.push(1);
 
-    println!("{:?}", b);
+    // println!("{:?}", b);
+
+    let foo = RSEnum::Foo(5);
+
+    // pattern matching: wow
+    if let RSEnum::Foo(value) = foo {}
+
+    match foo {
+        RSEnum::Foo2(Some(value)) => {}
+        RSEnum::Foo2(None) => {}
+        RSEnum::Foo(value) => {}
+        _ => {}
+    }
 }
