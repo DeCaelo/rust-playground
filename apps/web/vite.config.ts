485572c3a20f98e1f31c48801ecc2663eacf9fab
DeCaelo <ludovic.cleuet@gmail.com>
2022-07-04 14:00:54 +0200

    react app tsx + vite

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,29 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import svg from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-plugin-tsconfig-paths';
+
+import { name, version } from './package.json';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	server: {
+		port: 8002
+	},
+	plugins: [
+		// @ts-ignore
+		react({
+			jsxRuntime: 'classic'
+		}),
+		svg({ svgrOptions: { icon: true } }),
+		tsconfigPaths()
+	],
+	root: 'src',
+	define: {
+		pkgJson: { name, version }
+	},
+	build: {
+		outDir: '../dist',
+		assetsDir: '.'
+	}
+});
